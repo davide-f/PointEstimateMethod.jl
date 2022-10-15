cec0ccfa2a2a6f6346eac18b6efb7ef381d84b02
Davide Fioriti <fioritidavidesubs@gmail.com>
2022-10-15 19:11:47 +0200

    Add copyright

--- src/pem.jl
+++ src/pem.jl
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: : 2022 Davide Fioriti
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# coding: utf-8
+
 """
     pem(d, N; std_fun=std, central_moment_fun=moment)
 
