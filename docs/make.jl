35c6f8f747cb656fed799f3cd4e77b4c42557e52
Davide Fioriti <fioritidavidesubs@gmail.com>
2026-04-28 23:13:26 +0200

    Revise examples with literate

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,60 @@
 using PointEstimateMethod
 using Documenter
+using Literate
+
+const _EXAMPLE_DIR = joinpath(@__DIR__, "src", "examples")
+
+"""
+    _include_sandbox(filename)
+Include the `filename` in a temporary module that acts as a sandbox. (Ensuring
+no constants or functions leak into other files.)
+"""
+function _include_sandbox(filename)
+    mod = @eval module $(gensym()) end
+    return Base.include(mod, filename)
+end
+
+function _file_list(full_dir, relative_dir, extension)
+    return map(
+        file -> joinpath(relative_dir, file),
+        filter(file -> endswith(file, extension), sort(readdir(full_dir))),
+    )
+end
+
+function link_example(content)
+    edit_url = match(r"EditURL = \"(.+?)\"", content)[1]
+    footer = match(r"^(---\n\n\*This page was generated using)"m, content)[1]
+    content = replace(
+        content, footer => "!!! info\n    [View this file on Github]($(edit_url)).\n\n" * footer
+    )
+    return content
+end
+
+function literate_directory(dir)
+    rm.(_file_list(dir, dir, ".md"))
+    for filename in _file_list(dir, dir, ".jl")
+        # `include` the file to test it before `#src` lines are removed. It is
+        # in a testset to isolate local variables between files.
+        @testset "$(filename)" begin
+            _include_sandbox(filename)
+        end
+        Literate.markdown(
+            filename,
+            dir;
+            documenter = true,
+            postprocess = link_example
+        )
+    end
+    return
+end
+
+literate_directory(_EXAMPLE_DIR)
+
+examples = [
+    "normal_distribution",
+    "general_distribution",
+    "multivariate_distribution",
+]
 
 makedocs(
     modules = [PointEstimateMethod],
@@ -16,9 +71,8 @@ makedocs(
             "installation.md",
         ],
         "Examples" => [
-            "examples/normal_distribution.md",
-            "examples/general_distribution.md",
-            "examples/multivariate_distribution.md",
+            joinpath("examples", f * ".md")
+            for f in examples
         ],
         "API Reference" => "api_reference.md",
     ]
